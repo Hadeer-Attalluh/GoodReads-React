d52b54bc99ae8d58ebd6def55e99a834a6f7d0b0
hadeer-attalluh <hadeermohamed.fcis@gmail.com>
2019-03-29 14:19:14 +0200

    add book in admin panel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import AuthorsData from '../src/Data/Authers';
-import AuthorsAdminListing from '../src/FeaturedComponents/Admin/Auther/Listing';
+
+import AuthorsData from '../src/Data/Authors';
+import AuthorsAdminListing from '../src/FeaturedComponents/Admin/Author/Listing';
 import UsersData from './Data/Users.json';
+
 import { SignUpForm } from './FeaturedComponents/UserForms/signup';
 
 import BooksData from '../src/Data/Books';
 import BooksDisplayListing from '../src/FeaturedComponents/Featured/Book/Listing';
 import BookDetails from '../src/FeaturedComponents/Featured/Book/Details';
 
+import AdminPanel from './FeaturedComponents/Admin/Panel';
+import BooksAdminListing from './FeaturedComponents/Admin/Books/Listing';
+
 export const context = React.createContext();
+
 class App extends Component {
   constructor() {
     super();
@@ -20,6 +26,11 @@ class App extends Component {
     this.addAuthor = this.addAuthor.bind(this);
     this.editAuthor = this.editAuthor.bind(this);
     this.deleteAuthor = this.deleteAuthor.bind(this);
+
+    this.addBook = this.addBook.bind(this);
+    this.editBook = this.editBook.bind(this);
+    this.deleteBook = this.deleteBook.bind(this);
+
     this.addUser = this.addUser.bind(this);
   }
 
@@ -41,10 +52,27 @@ class App extends Component {
     newAuthors.find(a => a.id === authorID).deleted = true
     this.setState({ Authors: [...newAuthors] });
   }
-  
+
+  addBook(book) {
+    // const newAuthors = this.state.Authors.slice();
+    // this.setState({ Authors: [...newAuthors.concat(author)] });
+  }
+  editBook(book) {
+    // console.log(author);
+    // const newAuthors = this.state.Authors.slice();
+    // let authorEdited = newAuthors.findIndex(a => a.id === author.id);
+    // newAuthors[authorEdited] = author;
+    // this.setState({ Authors: [...newAuthors] });
+  }
+  deleteBook(bookID) {
+    const newbooks = this.state.Books.slice();
+    newbooks.find(b => b.id === bookID).deleted = true
+    this.setState({ Books: [...newbooks] });
+  }
+
   addUser(user) {
     const newUsers = this.state.Users.slice();
-    this.setState({Users: [...newUsers.concat(user)]});
+    this.setState({ Users: [...newUsers.concat(user)] });
   }
 
   render() {
@@ -55,14 +83,20 @@ class App extends Component {
       deleteAuthor: this.deleteAuthor,
 
       books: this.state.Books,
+      addBook: this.addBook,
+      editBook: this.editBook,
+      deleteBook: this.deleteBook,
+
       addUser: this.addUser,
     }
     return (
       <context.Provider value={contextValue}>
-      <SignUpForm/>
+        {/* <SignUpForm/> */}
+        <AdminPanel />
         {/* <AuthorsAdminListing /> */}
+        <BooksAdminListing />
         {/* <BooksDisplayListing /> */}
-        <BookDetails {...contextValue.books[1]} />
+        {/* <BookDetails {...contextValue.books[1]} /> */}
       </context.Provider>
     );
   }
