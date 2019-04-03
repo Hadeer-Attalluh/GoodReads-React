b38b2fb2ad28f91b6020ca7ff2fb18ebcb1bf27d
hadeer-attalluh <hadeermohamed.fcis@gmail.com>
2019-04-03 17:59:15 +0200

    styling

--- src/FeaturedComponents/Featured/Book/Details.js
+++ src/FeaturedComponents/Featured/Book/Details.js
@@ -10,12 +10,6 @@ export default class BookDetails extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            // id: "",
-            // authorId: "",
-            // categoryId: "",
-            // title: "",
-            // cover: "",
-            // deleted: false
         }
     }
     componentWillMount() {
@@ -33,41 +27,76 @@ export default class BookDetails extends React.Component {
             <context.Consumer>
                 {
                     value => (
-                        <Container fluid={false} className="d-flex flex-column m-3 p-3">
-                            <div className="d-flex">
-                                <div className="d-flex flex-column">
-                                    <Card>
-                                        <Card.Img variant="top" src={this.state.cover === "" ? "http://kalaharilifestyle.com/wp-content/uploads/2014/04/placeholder4.png" : require('../../../Assets/Books/' + this.state.cover)} alt="Book Cover" />
-                                    </Card>
-                                    <Form.Group as={Col} className="d-block">
-                                        <Form.Control as="select">
-                                            <option>Read</option>
-                                            <option>Currently Reading</option>
-                                            <option>Want To Read</option>
-                                        </Form.Control>
-                                    </Form.Group>
+                        <Container fluid={false} className="">
+                            <Row>
+                                <Col xs={4}>
+                                    <Row className="no-gutters m-2">
+                                        <Col xs={12}>
+                                            <Card>
+                                                <Card.Img variant="top" src={this.state.cover === "" ? "http://kalaharilifestyle.com/wp-content/uploads/2014/04/placeholder4.png" : require('../../../Assets/Books/' + this.state.cover)} alt="Book Cover" />
+                                            </Card>
+                                        </Col>
+                                    </Row>
+                                    <Row className="text-center no-gutters m-2">
+                                        <Col xs={12}>
+                                            <Rating emptySymbol="far fa-star" fullSymbol="fas fa-star text-warning" />
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col xs={12}>
+                                            <Form.Group as={Col} className="d-block">
+                                                <Form.Control as="select">
+                                                    <option>Read</option>
+                                                    <option>Currently Reading</option>
+                                                    <option>Want To Read</option>
+                                                </Form.Control>
+                                            </Form.Group>
+                                        </Col>
+                                    </Row>
 
-                                </div>
-                                <Card className="border-0">
-                                    <Card.Body>
-                                        <Card.Title>{this.state.title}</Card.Title>
-                                        <Card.Subtitle className="mb-2 text-muted">By
+                                </Col>
+                                <Col xs={8}>
+                                    <Card className="border-0">
+                                        <Card.Body>
+                                            <Card.Title>{this.state.title}</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">By
                                             <Link to={`/authors/${this.state.authorId}`}>{' ' + this.getAuthorFullName(value.authors, this.state.authorId)}</Link>
-                                        </Card.Subtitle>
-                                        <Card.Subtitle className="mb-2 text-muted">
-                                            <Link to={`/categories/${this.state.categoryId}`}>{' ' + value.Categories.find(a => a.id === this.state.categoryId).name}</Link>
-                                        </Card.Subtitle>
-                                        <Rating readonly initialRating={4} emptySymbol="far fa-star" fullSymbol="fas fa-star text-warning" />
-                                        <Card.Text> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-                            <div>
-                                <Card >
-                                    <Card.Header>Reviews</Card.Header>
-                                    <ListGroup variant="li">
-                                        <ListGroup.Item>
-                                            <Media>
+                                            </Card.Subtitle>
+                                            <Card.Subtitle className="mb-2 text-muted">
+                                                <Link to={`/categories/${this.state.categoryId}`}>{' ' + value.Categories.find(a => a.id === this.state.categoryId).name}</Link>
+                                            </Card.Subtitle>
+                                            <Card.Text className="text-muted">Published 2010</Card.Text>
+                                            <Rating readonly initialRating={4} emptySymbol="far fa-star" fullSymbol="fas fa-star text-warning" />
+                                            <Card.Text> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12}>
+                                    <Card >
+                                        <Card.Header className="bg-dark text-center text-light">Reviews</Card.Header>
+                                        <ListGroup variant="li">
+                                            <ListGroup.Item>
+                                                <Media>
+                                                    <img
+                                                        width={64}
+                                                        height={64}
+                                                        className="align-self-start mr-3"
+                                                        src="http://kalaharilifestyle.com/wp-content/uploads/2014/04/placeholder4.png"
+                                                        alt="Generic placeholder"
+                                                    />
+                                                    <Media.Body>
+                                                        <h5>Review 1</h5>
+                                                        <p>
+                                                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                                                            ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                                                            tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                                            fringilla. Donec lacinia congue felis in faucibus.</p>
+                                                    </Media.Body>
+                                                </Media>
+                                            </ListGroup.Item>
+                                            <ListGroup.Item><Media>
                                                 <img
                                                     width={64}
                                                     height={64}
@@ -76,35 +105,18 @@ export default class BookDetails extends React.Component {
                                                     alt="Generic placeholder"
                                                 />
                                                 <Media.Body>
-                                                    <h5>Review 1</h5>
+                                                    <h5>Review 2</h5>
                                                     <p>
                                                         Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
                                                         ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
                                                         tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-                                            fringilla. Donec lacinia congue felis in faucibus.</p>
-                                                </Media.Body>
-                                            </Media>
-                                        </ListGroup.Item>
-                                        <ListGroup.Item><Media>
-                                            <img
-                                                width={64}
-                                                height={64}
-                                                className="align-self-start mr-3"
-                                                src="http://kalaharilifestyle.com/wp-content/uploads/2014/04/placeholder4.png"
-                                                alt="Generic placeholder"
-                                            />
-                                            <Media.Body>
-                                                <h5>Review 2</h5>
-                                                <p>
-                                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-                                                    ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-                                                    tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
                                         fringilla. Donec lacinia congue felis in faucibus. </p>
-                                            </Media.Body>
-                                        </Media></ListGroup.Item>
-                                    </ListGroup>
-                                </Card>
-                            </div>
+                                                </Media.Body>
+                                            </Media></ListGroup.Item>
+                                        </ListGroup>
+                                    </Card>
+                                </Col>
+                            </Row>
                         </Container >
                     )
                 }
--- src/FeaturedComponents/Featured/Book/Listing.js
+++ src/FeaturedComponents/Featured/Book/Listing.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { CardDeck, Container, Col } from 'react-bootstrap';
+import { CardDeck, Container, Col, Row, Card } from 'react-bootstrap';
 import { context } from '../../../App';
-
+import Paging from '../../User/Pagination';
 import BookDisplayCard from './Card';
 export default class BookDisplayListing extends React.Component {
     render() {
@@ -10,9 +10,22 @@ export default class BookDisplayListing extends React.Component {
                 {
                     value => (
                         <Container fluid={true}>
-                            <CardDeck>
-                                {value.books.filter(b => !b.deleted).map(b => <Col sm={4} md={3} className="my-3" key={b.id}><BookDisplayCard {...b} /></Col>)}
-                            </CardDeck>
+                            <Row>
+                                <Col xs={12}>
+                                    <Card>
+                                        <Card.Img variant="Top" src={require('../../../Assets/banner22.jpg')} />
+                                        <Card.Header className="text-center bg-dark text-light">Books Colloections</Card.Header>
+                                    </Card>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <CardDeck>
+                                    {value.books.filter(b => !b.deleted).map(b => <Col sm={4} md={3} className="my-3" key={b.id}><BookDisplayCard {...b} /></Col>)}
+                                </CardDeck>
+                            </Row>
+                            <Row className="text-center">
+                                <Paging />
+                            </Row>
                         </Container>
                     )
                 }
