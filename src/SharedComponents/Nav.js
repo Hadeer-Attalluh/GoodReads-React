48100ebb7c4461022d96cb957178bc0eae326cca
hadeer-attalluh <hadeermohamed.fcis@gmail.com>
2019-04-29 12:59:29 +0200

    add searching - not working-

--- src/SharedComponents/Nav.js
+++ src/SharedComponents/Nav.js
@@ -2,29 +2,52 @@
 import React from 'react';
 import { Navbar, Nav, Form, FormControl } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import { server } from '../API/Book';
 
-const UserNavBar = (props) => {
-	return (
-		<Navbar collapseOnSelect expand="lg"  fixed="top" className="Navbar" >
-			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-			<Navbar.Collapse id="responsive-navbar-nav">
-				<Nav className="mr-auto">
-					<Nav.Link as="div" ><Link to="/" className="link-white"> Home </Link> </Nav.Link>
-					<Nav.Link as="div" ><Link to="/categories" className="link-white"> Categories </Link> </Nav.Link>
-					<Nav.Link as="div" ><Link to="/books" className="link-white"> Books </Link> </Nav.Link>
-					<Nav.Link as="div" ><Link to="/authors" className="link-white"> Authors </Link> </Nav.Link>
-				</Nav>
-				<Form inline>
-					<FormControl type="text" placeholder="Search" className="mr-sm-2 search" />
-				</Form>
-				{/* <h3><i className="fas fa-user"></i></h3>
+class UserNavBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchKey: ""
+		}
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleChange(e) {
+		this.setState({ [e.target.name]: e.target.value });
+	}
+	handleSubmit(e) {
+		e.perventDefault();
+		axios.get(`${server}/api/books/search`, {
+			searchKey: this.state.searchKey
+		}).then(res => {
+			// ??
+		})
+	}
+	render() {
+		return (
+			<Navbar collapseOnSelect expand="lg" fixed="top" className="Navbar" >
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link as="div" ><Link to="/" className="link-white"> Home </Link> </Nav.Link>
+						<Nav.Link as="div" ><Link to="/categories" className="link-white"> Categories </Link> </Nav.Link>
+						<Nav.Link as="div" ><Link to="/books" className="link-white"> Books </Link> </Nav.Link>
+						<Nav.Link as="div" ><Link to="/authors" className="link-white"> Authors </Link> </Nav.Link>
+					</Nav>
+					<Form inline onSubmit={this.handleSubmit}>
+						<FormControl type="text" name="searchKey" onchange={this.handleChange} placeholder="Search" className="mr-sm-2 search" />
+					</Form>
+					{/* <h3><i className="fas fa-user"></i></h3>
 				<h3>user 1</h3>
 				<h3><i className="fas fa-arrow-alt-circle-right"></i></h3> */}
-				<Nav.Link as="div" >
-					<Link className="login-btn" to="/login">Login </Link>
-				</Nav.Link>
-			</Navbar.Collapse>
-		</Navbar>
-	)
+					<Nav.Link as="div" >
+						<Link className="login-btn" to="/login">Login </Link>
+					</Nav.Link>
+				</Navbar.Collapse>
+			</Navbar>
+		);
+	}
 }
 export default UserNavBar;
